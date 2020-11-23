2c182cb193ae060a8dbd05150d0a6f0d9ff53777
Min Hou <minhou@inglab.com.my>
2020-11-23 12:02:01 +0800

    Initial Commit

---
+++ Movie MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie MVVM
+//
+//  Created by Lai Min Hou on 23/11/2020.
+//  Copyright © 2020 IngLab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
